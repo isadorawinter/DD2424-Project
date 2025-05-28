503104a570de73e769e89d7528dbb1828fbf6911
josefinahakkinen <43602308+josefinahakkinen@users.noreply.github.com>
2025-05-28 15:18:39 +0200

    Update RNN.py

--- RNN/RNN.py
+++ RNN/RNN.py
@@ -1,11 +1,10 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import SimpleRNN, Dense, Embedding
+from tensorflow.keras.layers import SimpleRNN, Dense, Embedding, LayerNormalization, Dropout
 from DataHandler import DataHandler
 class RNN:
     def build_model(self, batch_size, sequence_length, vocab_size, embedding_dim, n_rnn_units, learning_rate, num_layers):
-        # Creates a Keras Sequential model,meaning the layers are stacked
-        # from input to output.
+       
         inputs = tf.keras.Input(
             batch_shape=(batch_size, sequence_length), dtype=tf.int32
         )
@@ -15,13 +14,16 @@ class RNN:
             output_dim=embedding_dim
         )(inputs)
 
-        x = tf.keras.layers.SimpleRNN(
-            units=n_rnn_units,
-            return_sequences=True,
-            stateful=True,
-            kernel_initializer='he_uniform',
-            recurrent_initializer='glorot_uniform'
-        )(x)
+        for i in range(num_layers):
+            x = tf.keras.layers.SimpleRNN(
+                units=n_rnn_units,
+                return_sequences=True,  
+                stateful=True,  
+                kernel_initializer='he_uniform',
+                recurrent_initializer='glorot_uniform'
+            )(x)
+        x = LayerNormalization(epsilon=1e-8)(x)  
+        x = Dropout(0.15)(x)  
 
         outputs = tf.keras.layers.Dense(vocab_size)(x)
 
