5a835bb9da743d8d97fd48490a5dc544c8fc57f7
josefinahakkinen <43602308+josefinahakkinen@users.noreply.github.com>
2025-05-28 16:41:28 +0200

    Create GRU.py

---
+++ RNN/GRU.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import GRU, Dense, Embedding
+from DataHandler import DataHandler
+
+
+class GRU:
+    def build_model(self, batch_size, sequence_length, vocab_size, embedding_dim, n_rnn_units, learning_rate, num_layers):
+       
+       
+        inputs = tf.keras.Input(
+            batch_shape=(batch_size, sequence_length), dtype=tf.int32
+        )
+
+
+        x = tf.keras.layers.Embedding(
+            input_dim=vocab_size,
+            output_dim=embedding_dim
+        )(inputs)
+       
+        for _ in range(num_layers):
+            x = tf.keras.layers.GRU(
+                units=n_rnn_units,
+                return_sequences=True,
+                stateful=True,
+                kernel_initializer='he_uniform',
+                recurrent_initializer='glorot_uniform'
+            )(x)
+
+
+        outputs = tf.keras.layers.Dense(vocab_size)(x)
+
+
+        model = tf.keras.Model(inputs, outputs)
+
+
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        model.compile(
+            optimizer=optimizer,
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        )
+
+
+        model.summary()
+        return model
